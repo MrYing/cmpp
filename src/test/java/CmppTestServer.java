af046d99d533a533f511aec40df9a570ef9882de
crossoverJie <crossoverjie@gmail.com>
2017-10-23 15:26:32 +0800

    注释

--- src/test/java/CmppTestServer.java
+++ src/test/java/CmppTestServer.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 public class CmppTestServer {
 	private final static Logger LOGGER = LoggerFactory.getLogger(LoggerFactory.class);
 	public static void main(String args[]) {
-		new CmppServer(17090).start();
+		new CmppServer(7890).start();
 		LOGGER.debug("app start") ;
 	}
 }
